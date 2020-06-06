7bf3aaa256392a6a07f4f42edc1aec3304b0203a
System Administrator <roarijo89@gmail.com>
2020-06-06 02:41:18 -0400

    c

---
+++ bew/index.js
@@ -0,0 +1,36 @@
+const userEmail = document.getElementById('user-email');
+const userPass = document.getElementById('user-password');
+const loginBtn = document.querySelector('.login-btn');
+
+
+
+loginBtn.addEventListener('click',()=>{
+    if(userEmail.value =='' || userPass.value ==''){
+        alert('Plz Fill All Fields')
+    }
+  
+    else{
+    var email = userEmail.value;
+    var pass = userPass.value;
+
+    var data = {email,pass};
+    
+
+    var options = {
+        method:'POST',
+        body:JSON.stringify(data),
+        headers:{
+            'Content-type':'application/json'
+        }
+        }
+
+    fetch('/login',options).then(res=>{return res.text()}).then(data=>document.write(data))
+    }
+    
+})
+
+
+
+
+
+
