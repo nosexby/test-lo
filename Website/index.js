95d15937ec3cfce89217b18d50d713a46428d3a6
Glitch (highfalutin-auspicious-beef) <none>
2020-07-22 09:20:57 +0000

    🌷🚒 Updated with Glitch

--- Website/index.js
+++ Website/index.js
@@ -1,36 +1,28 @@
-const userEmail = document.getElementById('user-email');
-const userPass = document.getElementById('user-password');
-const loginBtn = document.querySelector('.login-btn');
+const userEmail = document.getElementById("user-email");
+const userPass = document.getElementById("user-password");
+const loginBtn = document.querySelector(".login-btn");
 
-
-
-loginBtn.addEventListener('click',()=>{
-    if(userEmail.value =='' || userPass.value ==''){
-        alert('Plz Fill All Fields')
-    }
-  
-    else{
+loginBtn.addEventListener("click", () => {
+  if (userEmail.value == "" || userPass.value == "") {
+    alert("Plz Fill All Fields");
+  } else {
     var email = userEmail.value;
     var pass = userPass.value;
 
-    var data = {email,pass};
-    
+    var data = { email, pass };
 
     var options = {
-        method:'POST',
-        body:JSON.stringify(data),
-        headers:{
-            'Content-type':'application/json'
-        }
-        }
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json"
+      }
+    };
 
-    fetch('/login',options).then(res=>{return res.text()}).then(data=>document.write(data))
-    }
-    
-})
-
-
-
-
-
-
+    fetch("/login", options)
+      .then(res => {
+        return res.text();
+      })
+      .then(data => document.write(data));
+  }
+});
